f17125d0b606a26ab9a35900878301e3e354e6ec
Changwon Lee <89180067+LeeChangwon-AT-SCH@users.noreply.github.com>
2022-10-21 21:21:38 +0900

    Update calIndexSearch.py

--- calIndexSearch.py
+++ calIndexSearch.py
@@ -4,29 +4,29 @@ from operator import length_hint
 def calIndexSearch(s_m, s_d, a_m, a_d):
     index = 0
     if a_m - 1 == s_m:
-        if s_m == 7 or 8 or 10 or 12 or 1:
+        if s_m == 7 or s_m == 8 or s_m == 10 or s_m == 12 or s_m == 1:
             index = 31 - s_d + a_d
             return index
         else:
             index = 30 - s_d + a_d
             return index      
     elif a_m - 2 == s_m:
-        if s_m == 7 or 8 or 10 or 12 or 1:
-            if a_m - 1 == 7 or 8 or 10 or 12 or 1:    
+        if s_m == 7 or s_m == 8 or s_m == 10 or s_m == 12 or s_m == 1:
+            if a_m - 1 == 7 or a_m - 1 == 8 or a_m - 1 == 10 or a_m - 1 == 12 or a_m - 1 == 1:    
                 index = 31 - s_d + a_d + 31
                 return index
             else:
                 index = 31 - s_d + a_d + 30
                 return index
         else:
-            if a_m - 1 == 7 or 8 or 10 or 12 or 1:    
+            if a_m - 1 == 7 or a_m - 1 == 8 or a_m - 1 == 10 or a_m - 1 == 12 or a_m - 1 == 1:    
                 index = 30 - s_d + a_d + 31
                 return index
             else:
                 index = 30 - s_d + a_d + 30
                 return index            
     elif a_m == s_m:
-        if s_m == 7 or 8 or 10 or 12 or 1:
+        if s_m == 7 or s_m == 8 or s_m == 10 or s_m == 12 or s_m == 1:
             index = a_d - s_d
             return index
         else:
@@ -36,7 +36,7 @@ def calIndexSearch(s_m, s_d, a_m, a_d):
 
 def listLength(s_m, s_d, this_month, today):
     if this_month - 1 == s_m:
-        if s_m == 7 or 8 or 10 or 12 or 1:
+        if s_m == 7 or s_m == 8 or s_m == 10 or s_m == 12 or s_m == 1:
             length = 31 - s_d + today
             return length + 1
         else:
@@ -44,7 +44,7 @@ def listLength(s_m, s_d, this_month, today):
             return length + 1
 
     elif this_month == s_m:
-        if s_m == 7 or 8 or 10 or 12 or 1:
+        if s_m == 7 or s_m == 8 or s_m == 10 or s_m == 12 or s_m == 1:
             length = today - s_d
             return length + 1
 
